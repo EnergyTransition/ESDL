ffc7434f9daf2cbdaad7a789dba6a49d690a9261
Ewoud Werkman <ewoud.werkman@tno.nl>
2024-05-02 13:50:22 +0200

    Improve CDO version script

--- esdl.parent/set_cdo_version.sh
+++ esdl.parent/set_cdo_version.sh
@@ -1,2 +1,3 @@
 # updates the pom.xml and Manifest.mf files to a cdo version. E.g. 2.21.9 becomes 2.21.9.cdo
 mvn build-helper:parse-version org.eclipse.tycho:tycho-versions-plugin:set-version -Dtycho.mode=maven -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.cdo
+mvn build-helper:parse-version org.eclipse.tycho:tycho-versions-plugin:update-eclipse-metadata 
