a39b2ee693ecbef0a289e8601c35917ae4c14ac2
Ewoud Werkman <ewoud.werkman@tno.nl>
2026-02-09 16:12:48 +0100

    Update set version scripts to match new Maven Tycho features

--- esdl.parent/set_version.sh
+++ esdl.parent/set_version.sh
@@ -3,4 +3,4 @@ if [ -z $1 ]; then
 	echo "Usage $0 <version>, e.g. 1.21.1"
 	exit 0
 fi
-mvn -Dtycho.mode=maven -DnewVersion=$1 org.eclipse.tycho:tycho-versions-plugin:set-version
+mvn -DnewVersion=$1 org.eclipse.tycho:tycho-versions-plugin:set-version
